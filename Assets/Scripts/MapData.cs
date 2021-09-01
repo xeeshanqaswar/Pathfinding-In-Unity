b16e30cc87a3f854ebc9147e5d8e59fb8196d4d2
HASHIRSIDDIQI-P\Zeeshan <qaswarzeeshan@gmail.com>
2021-09-01 15:20:37 +0500

    Imported PSD Texture file from Photoshop to Create Maze Implemented Methods to get Maze Data from Texture2D (PSD).
    Completed : Implementation of Maze Generation.

--- Assets/Scripts/MapData.cs
+++ Assets/Scripts/MapData.cs
@@ -7,14 +7,29 @@ public class MapData : MonoBehaviour
 {
     public int width = 10;
     public int height = 5;
-    public TextAsset mazeData;
 
-    public List<string> GetTextFromFile()
+    public TextAsset mazeData;
+    public Texture2D textureMap;
+    public string ResourcePath = "Mapdata";
+
+    private void Awake()
     {
-        return GetTextFromFile(mazeData);
+        if (textureMap == null)
+        {
+            textureMap = Resources.Load(ResourcePath + "/TextureMaze") as Texture2D;
+        }
+        if (mazeData == null)
+        {
+            mazeData = Resources.Load(ResourcePath + "/DemoMaze") as TextAsset;
+        }
     }
 
-    public List<string> GetTextFromFile(TextAsset tAsset)
+    public List<string> GetMapFromTextfile()
+    {
+        return GetMapFromTextfile(mazeData);
+    }
+
+    public List<string> GetMapFromTextfile(TextAsset tAsset)
     {
         List<string> lines = new List<string>();
 
@@ -28,6 +43,39 @@ public class MapData : MonoBehaviour
         else
         {
             Debug.Log("MAPDATA GetTextFromFile Error: invalid TextAsset");
+        }
+
+        return lines;
+    }
+
+    public List<string> GetMapfromTexture()
+    {
+        return GetMapfromTexture(textureMap);
+    }
+
+    public List<string> GetMapfromTexture(Texture2D texture)
+    {
+        List<string> lines = new List<string>();
+        for (int y = 0; y < texture.height; y++)
+        {
+            string line = "";
+            for (int x = 0; x < texture.width; x++)
+            {
+                if (texture.GetPixel(x, y) == Color.black)
+                {
+                    line += "1";
+                }
+                else if(texture.GetPixel(x, y) == Color.white)
+                {
+                    line += "0";
+                }
+                else
+                {
+                    line += " ";
+                }
+            }
+
+            lines.Add(line);
         }
 
         return lines;
@@ -48,7 +96,16 @@ public class MapData : MonoBehaviour
     public int[,] MakeMap()
     {
         List<string> lines = new List<string>();
-        lines = GetTextFromFile();
+
+        if (textureMap != null)
+        {
+            lines = GetMapfromTexture();
+        }
+        else
+        {
+            lines = GetMapFromTextfile();
+        }
+
         SetDimensions(lines);
 
         int[,] map = new int[width, height];
